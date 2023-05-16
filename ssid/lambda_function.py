412d1431c288816b5e3044540c09d5ab7d3f52dc
nano-app <133526287+nano-app@users.noreply.github.com>
2023-05-16 09:02:09 +0700

    Create lambda_function.py
    (test run ok)
    - layer zip (pip install target: qrcode, requests, boto3..)
    - lambda configurtion: environment key, url, ... exec time..

---
+++ ssid/lambda_function.py
@@ -0,0 +1,65 @@
+import json
+import boto3
+import requests
+import qrcode
+import os
+
+# funct. upload file to s3
+def upload_file_to_s3(file_path, bucket_name):
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(bucket_name)
+    file_name = file_path.split('/')[-1]  # Extracting just the file name from the file path
+    
+    try:
+        bucket.upload_file(file_path, file_name)
+        print(f"File '{file_name}' uploaded successfully to S3 bucket '{bucket_name}'.")
+    except Exception as e:
+        print(f"Error uploading file '{file_name}' to S3 bucket '{bucket_name}': {str(e)}")
+
+# funct. generate qr from input string and upload image to s3
+def gen_qr_and_upload_s3(source_string, bucket_name):
+    
+    img = qrcode.make(source_string)
+    img.save('/tmp/outputqr.png')    
+    file_path = '/tmp/outputqr.png'
+    upload_file_to_s3(file_path, bucket_name)
+
+# MAIN function: request ssid info, parsing json result, call qr generating and upload to s3
+def lambda_handler(event, context):
+    # TODO implement
+    bucket_name = os.environ['BUCKET_NAME']
+    my_api_key = os.environ['API_KEY']
+    ssid_url = os.environ['API_SSID_URL']
+    
+    #1. Get ssid info
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "X-Cisco-Meraki-API-Key": my_api_key
+        }
+    payload = None
+    
+    try:
+        response = requests.request('GET', ssid_url, headers=headers, data = payload)
+        print(response)
+        
+        # 2. gen qr and upload to s3
+        # extract ssid info from json result
+        json_data = json.loads(response.text.encode('utf8'))
+        ssid_name = json_data['name']
+        ssid_psk = json_data['psk']
+        ssid_encmode = json_data['encryptionMode']
+        
+            # wifi string format 
+            # WIFI:S:<ssid_name>;T:WPA;P:<password>;H:false;;
+        ssid_string = 'WIFI:S:' + ssid_name + ';T:' + ssid_encmode+';P:' + ssid_psk + ';H:false;;'
+        gen_qr_and_upload_s3(ssid_string, bucket_name)
+        return {
+            'statusCode': 200,
+            'body': json.dumps('get ssid result: ' + ssid_string)
+        }
+    except Exception as e:
+        print(f"Error in main function': {str(e)}")
+
+        
+    
